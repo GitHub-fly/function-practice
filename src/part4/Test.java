140fda27844689e428cde9e2c2c7fe381ce3ee18
GitHub-fly <1224178565@qq.com>
2020-02-22 22:55:22 +0800

    初始化仓库

---
+++ src/part4/Test.java
@@ -0,0 +1,11 @@
+package part4;
+
+/**
+ * @author xunmi
+ * @ClassName Test
+ * @Description TODO
+ * @Date 2020/2/22
+ * @Version 1.0
+ **/
+public class Test {
+}
---
+++ src/part5/Test.java
@@ -0,0 +1,11 @@
+package part5;
+
+/**
+ * @author xunmi
+ * @ClassName Test
+ * @Description TODO
+ * @Date 2020/2/22
+ * @Version 1.0
+ **/
+public class Test {
+}
