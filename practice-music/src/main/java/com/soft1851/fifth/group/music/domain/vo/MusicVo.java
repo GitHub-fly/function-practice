49e5025dbc303fa1260035f2c63785a0d7c89140
flygit <1224178565@qq.com>
2020-04-04 00:03:33 +0800

    提交批量取消收藏音乐mapper

---
+++ practice-music/src/main/java/com/soft1851/fifth/group/music/domain/vo/MusicVo.java
@@ -0,0 +1,36 @@
+package com.soft1851.fifth.group.music.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author xunmi
+ * @ClassName MusicVo
+ * @Description TODO
+ * @Date 2020/4/3
+ * @Version 1.0
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MusicVo {
+    private Integer userId;
+    private Integer musicId;
+    private String title;
+    private String url;
+    private String singer;
+    private String cover;
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+    /**
+     * 用户积分
+     */
+    private Integer credits;
+}
--- practice-music/src/main/java/com/soft1851/fifth/group/music/mapper/MusicMapper.java
+++ practice-music/src/main/java/com/soft1851/fifth/group/music/mapper/MusicMapper.java
@@ -1,5 +1,11 @@
 package com.soft1851.fifth.group.music.mapper;
 
+import com.soft1851.fifth.group.music.domain.entity.Like;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * @author xunmi
  * @ClassName MusicMapper
@@ -7,5 +13,15 @@ package com.soft1851.fifth.group.music.mapper;
  * @Date 2020/4/3
  * @Version 1.0
  **/
+@Transactional(rollbackFor = Exception.class)
 public interface MusicMapper {
+
+    /**
+     * 批量取消收藏歌曲
+     * @param likes
+     * @return int
+     */
+    int batchCancel(@Param("likes") List<Like> likes);
+
+
 }
