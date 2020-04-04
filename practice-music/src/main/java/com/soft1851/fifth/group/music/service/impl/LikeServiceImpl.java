c7307f254e60451eb82c0f7d232a945ab13cbc4a
82043420@qq.com <82043420@qq.com>
2020-04-05 00:12:52 +0800

    关于用户

---
+++ practice-music/src/main/java/com/soft1851/fifth/group/music/service/impl/LikeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.soft1851.fifth.group.music.service.impl;
+
+/**
+ * @Author xuyuan
+ * @Date 2020/4/4 16:53
+ * @Version 1.0
+ */
+public class LikeServiceImpl {
+}
---
+++ practice-music/src/main/test/com/soft1851/fifth/group/music/mapper/LikeMapperTest.java
@@ -0,0 +1,38 @@
+package com.soft1851.fifth.group.music.mapper;
+
+import com.soft1851.fifth.group.music.domain.entity.Like;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author xuyuan
+ * @Date 2020/4/4 17:06
+ * @Version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring-mybatis.xml"})
+public class LikeMapperTest {
+    @Resource
+    private LikeMapper likeMapper;
+    @Test
+    public void insert(){
+        Like like=new Like();
+        like.setMusicId(5);
+        like.setUserId(4);
+        likeMapper.insert(like);
+
+    }
+
+    @Test
+    public void delete() {
+        likeMapper.delete(3);
+    }
+}
