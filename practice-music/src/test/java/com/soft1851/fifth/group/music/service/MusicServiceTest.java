f1547c0ebe990a3b6d1118a681f345e29663efa1
huangjingli <jingliqaq@gmail.com>
2020-04-05 14:05:43 +0800

    music的service层业务

---
+++ practice-music/src/test/java/com/soft1851/fifth/group/music/service/MusicServiceTest.java
@@ -0,0 +1,35 @@
+package com.soft1851.fifth.group.music.service;
+
+import com.soft1851.fifth.group.music.domain.entity.Like;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring-mybatis.xml"})
+public class MusicServiceTest {
+    @Resource
+    private MusicService musicService;
+
+    @Test
+    public void batchCancel() {
+        List<Like> likeList = new ArrayList<>();
+        for (int i = 1; i < 4; i++) {
+            likeList.add(Like.builder().userId(1).musicId(i).build());
+        }
+        musicService.batchCancel(likeList);
+    }
+
+    @Test
+    public void selectMusicInfo() {
+        Like like = Like.builder().musicId(20).build();
+        musicService.selectMusicInfo(like).forEach(System.out::println);
+    }
+}
