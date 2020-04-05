10ed4bbfb6db6f2c3aab4157d364a8b32467cee4
Tanxx <1255965856@qq.com>
2020-04-05 11:25:21 +0800

    修正

--- practice-music/src/test/java/com/soft1851/fifth/group/music/mapper/MusicMapperTest.java
+++ practice-music/src/test/java/com/soft1851/fifth/group/music/mapper/MusicMapperTest.java
@@ -1,5 +1,6 @@
 package com.soft1851.fifth.group.music.mapper;
 
+import com.soft1851.fifth.group.music.domain.entity.Like;
 import com.soft1851.fifth.group.music.util.MusicUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,6 +18,21 @@ import static org.junit.Assert.assertEquals;
 public class MusicMapperTest {
     @Resource
     private MusicMapper musicMapper;
+
+    @Test
+    public void batchCancel() {
+        List<Like> likes = new ArrayList<>(10);
+        for (int i = 0; i < 8; i++) {
+            likes.add(Like.builder().userId(1).musicId(i).build());
+        }
+        System.out.println(musicMapper.batchCancel(likes));
+    }
+
+    @Test
+    public void selectMusicInfo() {
+        Like like = Like.builder().userId(1).build();
+        musicMapper.selectMusicInfo(like).forEach(System.out::println);
+    }
 
     /**
      * 批量插入数据
